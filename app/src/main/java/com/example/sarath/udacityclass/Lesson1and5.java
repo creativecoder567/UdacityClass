c67c80a778f902ea0a4c2427ffdb345855a857c0
creativeoder567 <sarathcreativethinker@gmail.com>
2018-02-22 02:11:21 +0530

    Working on Lesson 6

---
+++ app/src/main/java/com/example/sarath/udacityclass/Lesson1and5.java
@@ -0,0 +1,13 @@
+package com.example.sarath.udacityclass;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Lesson1and5 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.lyla);
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/sarath/udacityclass/MainActivity.java
+++ app/src/main/java/com/example/sarath/udacityclass/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.sarath.udacityclass;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
+    TextView quantityTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        Button increment=findViewById(R.id.btnIncrement);
+        Button decrement=findViewById(R.id.btnDecrement);
+    }
+
+
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int quantity=2;
+        display(quantity);
+        displayPrice(quantity*5);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increment(View view) {
+        int quantity=3;
+        quantityTextView.setText(""+3);
+    }
+
+    public void decrement(View view) {
+        int quantity=1;
+        quantityTextView.setText(""+1);
     }
 }
